99893ea7dbbcb76542a918ace1ed8c168675952c
Chinna Namani <chinnanamani@Chinnas-MacBook-Air.local>
2022-02-19 15:52:58 +0530

    first  blog pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,28 @@
 module.exports = {
-    siteMetadata: {
-      title: `Blog-app`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `blog-app`,
+    author: {
+      name: `Naresh Namani`,
+      summary: `Blog to learning programming Languages and tricks`,
     },
+    description: `A starter blog demonstrating prgoramming lanaguages tricks.`,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    social: {
+      twitter: `kylemathews`,
+    },
+  },
     plugins: [
+      `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    
 
     ]
 }
