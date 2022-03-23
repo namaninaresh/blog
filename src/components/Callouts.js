f268fcd37a9eb4f45594d79e1c4de6ea16091463
Chinna Namani <chinnanamani@Chinnas-MacBook-Air.local>
2022-03-23 20:34:59 +0530

    added git fork sync blog

--- src/components/Callouts.js
+++ src/components/Callouts.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Callouts = ({name,children}) => 
 (
   
-    <div className={`callouts callouts-warning`}>
+    <div className={`callouts callouts-${name}`}>
             <p>{children}</p>
     </div>
 )
--- src/pages/about.js
+++ src/pages/about.js
@@ -17,34 +17,16 @@ const AboutPage = (props) => {
             <div className="about__pic-outline"><div className="about__pic"></div></div>
 
             <div className="about__description">
-              <p>
+              <p style={{textAlign:"center"}}>
                 Hi  Everyone, My name  is <span >Naresh Namani</span>
               </p>
               <p>
-                and can call me as <span>Chinna</span> voluptate tempor magna ea occaecat
-                pariatur. Tempor quis ex nisi officia ex reprehenderit labore
-                tempor irure deserunt cillum eu do. Reprehenderit aliquip
-                pariatur ad aute velit excepteur.
+               
+
+               Im a front end developer and I love to code the beautiful websites and learning new technologies
               </p>
               <p>
-                Ea elit ad dolore in excepteur occaecat irure adipisicing
-                officia do nostrud sint. Irure mollit ex ut aute mollit
-                excepteur veniam. Consectetur est dolore in ut. Nostrud
-                reprehenderit veniam amet ipsum aliqua. Enim reprehenderit
-                commodo id deserunt eiusmod ad velit laborum ipsum officia
-                nulla. Elit labore eu labore duis fugiat reprehenderit fugiat
-                consequat duis eu qui laborum. Excepteur adipisicing quis
-                nostrud qui. Magna adipisicing nulla cillum cillum ex sunt
-                cillum dolor sit ea ex. Elit sunt sint Lorem sit qui sunt. Nulla
-                voluptate ut sit id occaecat amet aliqua id. Voluptate
-                consectetur reprehenderit esse enim aute exercitation incididunt
-                voluptate id cupidatat mollit non. Laborum minim aute magna ea
-                sit exercitation amet esse minim enim. Qui cillum anim mollit
-                fugiat nisi occaecat. Laborum duis pariatur minim ex occaecat
-                dolore id reprehenderit non nostrud sunt deserunt esse Lorem.
-                Sunt eiusmod est occaecat nisi velit magna cillum Lorem occaecat
-                exercitation ipsum dolor veniam qui. Dolor elit veniam ullamco
-                qui aute non ullamco.
+                
               </p>
             </div>
           </div>
