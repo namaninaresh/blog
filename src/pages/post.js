bd97101ae16568aef8cc12a2291360a7bdbe3d8e
namaninaresh <namaninaresh1996@gmail.com>
2022-09-29 14:53:32 +0530

    added number systems,binary operations,binary addition 1's

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,50 +1,49 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql,Link} from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import {MDXProvider} from '@mdx-js/react'
-import Callouts from '../components/Callouts';
-import Accordation from '../components/Accordation'
-import Video from '../components/Video'
+import * as React from "react";
+import Layout from "../components/layout";
+import { graphql, Link } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import { MDXProvider } from "@mdx-js/react";
+import Callouts from "../components/Callouts";
+import Accordation from "../components/Accordation";
+import Video from "../components/Video";
 
-import TagIcon  from '../components/Icons/TagIcons';
-import Seo from '../components/Seo'
-const shortcodes = { Callouts,Accordation,Video}
+import TagIcon from "../components/Icons/TagIcons";
+import Seo from "../components/Seo";
+const shortcodes = { Callouts, Accordation, Video };
 
-const BlogPost = ({data,location , pageContext}) => {
-  
-
+const BlogPost = ({ data, location, pageContext }) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <Seo title={data.mdx.frontmatter.title} description={data.mdx.frontmatter.description} />
-   
-    <div className='blogPage '>
-     <div className='blogPage__header'>
-     <h1 className='blogPage__heading'>{data.mdx.frontmatter.title}</h1>
+      <Seo
+        title={data.mdx.frontmatter.title}
+        description={data.mdx.frontmatter.description}
+      />
 
-        {
-          data.mdx.frontmatter.tags.map((tag,index)=> (
-            <i className='tag__icons ph-1' key={index}>
-        <TagIcon name={tag} />
-        </i>
-          ))
-        }
-     
-      <p> Posted on : {data.mdx.frontmatter.date}</p>
-     {/* <p><Link to={pageContext.nextPostId} >{pageContext.nextPostId}</Link></p> */}
-     </div>
-    <hr></hr>
-    <MDXProvider components={shortcodes}>
-  <MDXRenderer>{data.mdx.body}</MDXRenderer>
-</MDXProvider>
-    </div>
-  </Layout>
-  )
-}
+      <div className="blogPage ">
+        <div className="blogPage__header">
+          <h1 className="blogPage__heading">{data.mdx.frontmatter.title}</h1>
+
+          {data.mdx.frontmatter.tags.map((tag, index) => (
+            <i className="tag__icons ph-1" key={index}>
+              <TagIcon name={tag} />
+            </i>
+          ))}
+
+          <p> Posted on : {data.mdx.frontmatter.date}</p>
+          {/* <p><Link to={pageContext.nextPostId} >{pageContext.nextPostId}</Link></p> */}
+        </div>
+        <hr></hr>
+        <MDXProvider components={shortcodes}>
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
+        </MDXProvider>
+      </div>
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: {eq: $id}) {
+    mdx(id: { eq: $id }) {
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
@@ -54,6 +53,5 @@ export const query = graphql`
       body
     }
   }
-`
-export default BlogPost
-
+`;
+export default BlogPost;
